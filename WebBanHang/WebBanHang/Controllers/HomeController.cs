0a084fdf923c47d665f21f851d22ec4c5b47bfe3
MinhTam-061199 <minhtam061199@gmail.com>
2021-04-30 00:38:47 -0700

    webbanhang

---
+++ WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Trangchu()
+        {
+            return View();
+        }
+
+        public IActionResult Sanphammoi()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult KhuyenMai()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Danhmucsanpham()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Oder()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+    }
+}
